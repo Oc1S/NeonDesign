dd0d3592cb48ea07f513f11b7565f0bdf8e12c9e
Oc1S <ct1147564685@gmail.com>
2022-02-21 18:41:19 +0800

    fix(component): remove dependency on animate.css

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -2,7 +2,7 @@ import React, { Fragment, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Button from '../button';
-import 'animate.css';
+import { Close } from '../icon';
 
 const prefix = 'neon-modal';
 
@@ -101,11 +101,11 @@ const Modal: React.FC<ModalProps> = ({
   };
 
   /* 处理类名 */
-  const modalClasses = classNames(`${prefix}`, modalClassName, 'animate__animated', {
-    animate__zoomIn: visible,
-    animate__zoomOut: !visible,
-    [`${prefix}-invisible`]: !visible,
-  });
+  const modalClasses = classNames(
+    `${prefix}`,
+    modalClassName,
+    visible ? `${prefix}-visible` : `${prefix}-invisible`,
+  );
   const titleClasses = classNames(`${prefix}-title`, titleClassName);
   const bodyClasses = classNames(`${prefix}-body`, bodyClassName);
   const maskClasses = classNames(`neon-mask`, maskClassName);
@@ -117,20 +117,7 @@ const Modal: React.FC<ModalProps> = ({
       ) : null}
 
       <div className={`${modalClasses}`} style={{ ...modalStyle, ...{ width, zIndex } }} {...rest}>
-        {closeable ? (
-          <span className="neon-icon-close" onClick={handleCancel}>
-            <svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="16" height="16">
-              <defs>
-                <style type="text/css"></style>
-              </defs>
-              <path
-                d="M556.8 512L832 236.8c12.8-12.8 12.8-32 0-44.8-12.8-12.8-32-12.8-44.8 0L512 467.2l-275.2-277.333333c-12.8-12.8-32-12.8-44.8 0-12.8 12.8-12.8 32 0 44.8l275.2 277.333333-277.333333 275.2c-12.8 12.8-12.8 32 0 44.8 6.4 6.4 14.933333 8.533333 23.466666 8.533333s17.066667-2.133333 23.466667-8.533333L512 556.8 787.2 832c6.4 6.4 14.933333 8.533333 23.466667 8.533333s17.066667-2.133333 23.466666-8.533333c12.8-12.8 12.8-32 0-44.8L556.8 512z"
-                p-id="3112"
-                fill="#ffffff"
-              ></path>
-            </svg>
-          </span>
-        ) : null}
+        {closeable ? <Close className="neon-icon-close" onClick={handleCancel} /> : null}
         {title && (
           <header className={titleClasses} style={titleStyle}>
             {title}
